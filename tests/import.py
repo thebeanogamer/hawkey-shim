b330677902489495bff2b2f91c03fd6c70a67749
Daniel Milnes <daniel@daniel-milnes.uk>
2023-09-17 15:16:28 +0100

    PoC Shim for DNF

--- tests/import.py
+++ tests/import.py
@@ -8,10 +8,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def test():
-    import libdnf
+    import dnf
 
     # sanity check
-    print("SHA256 of /etc/os-release:", libdnf.utils.checksum_value("sha256", "/etc/os-release"))
+    print("Age of /etc/os-release:", dnf.util.file_age("/etc/os-release"))
 
 
 if __name__ == "__main__":
